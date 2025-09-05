da7727dc9216c7556046490868ebe6f81aa3de54
Joshclxx <joshuacolobong11@gmail.com>
2025-09-05 15:56:31 +0800

    Set-Up: Initial commit for set-up system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,119 @@
-import Image from "next/image";
+"use client";
+
+import SectionContainer from "@/components/SectionContainer";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useUserSession } from "@/hooks/useUserSession";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const { loggedIn, userRole, setUser, logout } = useUserSession();
+
+  useEffect(() => {
+    if (loggedIn) {
+      if (userRole === "masterAdmin")
+        router.replace("/masterDashboard/adminDashboard");
+      else router.replace("/studentAccount");
+    }
+  }, [loggedIn, userRole, router]);
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const role = email.endsWith("@iihc.edu") ? "masterAdmin" : "student";
+
+    setUser(email, role, email, Date.now());
+
+    if (role === "masterAdmin")
+      router.replace("/masterDashboard/adminDashboard");
+    else router.replace("/studentAccount");
+  };
+
+  return (
+    <SectionContainer background="mt-1 h-auto">
+      <div className="flex items-center justify-center">
+        <img
+          src="/icons/iihc-logo.svg"
+          alt="IIH College Logo"
+          className="w-[240px] h-[240px]"
+        />
+      </div>
+
+      <div className="flex items-center justify-center">
+        <form
+          onSubmit={onSubmit}
+          className="w-[710px] h-[519px] bg-background rounded-lg shadow-2xl"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <p className="heading text-center mt-4">Login</p>
+
+          <div className="flex flex-col items-center justify-center mt-12">
+            <label className="flex flex-col justify-end caption gap-2">
+              <p className="font-bold">Email:</p>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                required
+                className="p-2 border rounded-lg w-[460px]"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
+              />
+            </label>
+
+            <label className="flex flex-col justify-end caption gap-2 mt-6">
+              <p className="font-bold">Password:</p>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                required
+                className="p-2 border rounded-lg w-[460px]"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+              />
+              <button
+                type="button"
+                onClick={logout}
+                className="caption underline text-left"
+              >
+                Forget Password?
+              </button>
+            </label>
+
+            <button
+              onClick={() => toast.success("Login Successful!")}
+              type="submit"
+              className="w-[224px] p-2 bg-button text-white button-text rounded-xl mt-12 hover:bg-button/85"
+            >
+              Login
+            </button>
+
+            <div className="flex flex-col items-center justify-center">
+              <div className="mt-5">
+                <div className="flex items-center gap-2">
+                  <div className="flex-1 w-14 h-[1px] bg-black"></div>
+                  <p className="caption">or continue with</p>
+                  <div className="flex-1 w-14 h-[1px] bg-black"></div>
+                </div>
+              </div>
+              <div className="container w-[224px] p-1 bg-background border-1 rounded-lg flex items-center justify-center gap-4 mt-5">
+                <img
+                  src="/icons/google-login.svg"
+                  alt="Google Login Icon"
+                  className="w-[20px] h-[20px]"
+                />
+                <button className="button-text text-foreground">
+                  Sign in with Email
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </SectionContainer>
   );
 }
