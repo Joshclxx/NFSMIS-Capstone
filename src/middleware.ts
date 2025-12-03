59bf77e172b09fbb482f36aa357b5c1ae1a72fc1
JaylordFPH <JaySoguilonIS@gmail.com>
2025-12-03 16:16:26 +0800

    fix: middleware configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PROTECTED_ROUTES = ["/"]; //dito josh lagay mo lang mga routes mo like ['/profile', '/dashboard']
-const AUTH_ROUTES = ["/login"];
-const SESSION_COOKIE_NAME = "session"; //change this later
+const PROTECTED_ROUTES = ["/admin/dashboard", "/profile", "/student"];
+const AUTH_ROUTES = ["/"];
+const SESSION_COOKIE_NAME = "session";
 
-export function authMiddleware(req: NextRequest) {
-  console.log("Im here at middleware handler")
+export default function middleware(req: NextRequest) {
+  console.log("Middleware is running...");
+
   const { pathname } = req.nextUrl;
   const sessionToken = req.cookies.get(SESSION_COOKIE_NAME)?.value;
 
   const isProtectedRoute = PROTECTED_ROUTES.some((route) =>
     pathname.startsWith(route)
   );
-  if (!isProtectedRoute || !sessionToken) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+
+  if (isProtectedRoute && !sessionToken) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
   const isAuthRoute = AUTH_ROUTES.some((route) => pathname.startsWith(route));
   if (isAuthRoute && sessionToken) {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl)); //change the dashboard if uts not the home pa ge
+    return NextResponse.redirect(new URL("/admin/dashboard", req.nextUrl));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next|.*\\..*).*)/"],
 };
